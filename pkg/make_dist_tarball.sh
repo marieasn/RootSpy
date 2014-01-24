7f43c476e08043ae63e5d11a1d04fa1e6784e7d2
sdobbs <sdobbs@e333a0bb-b310-0410-bd06-bbc7ae8e47d2>
2014-01-24 20:02:23 +0000

    now pull everything from svn
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/online/packages/RootSpy@12470 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- pkg/make_dist_tarball.sh
+++ pkg/make_dist_tarball.sh
@@ -5,19 +5,16 @@ TMPDIR=`mktemp -d` || exit 1
 echo "building in " $TMPDIR "..."
 
 # copy the base structure
-mkdir $TMPDIR/RootSpy
-cp -R * $TMPDIR/RootSpy/
+svn export https://halldsvn.jlab.org/repos/trunk/online/packages/RootSpy/pkg $TMPDIR/RootSpy
 
 # copy the packages
 svn export https://halldsvn.jlab.org/repos/trunk/online/packages/RootSpy $TMPDIR/RootSpy/RootSpy
 svn export https://halldsvn.jlab.org/repos/trunk/online/packages/DRootSpy $TMPDIR/RootSpy/DRootSpy
 
-#cp -R ../RootSpy-standalone/RootSpy $TMPDIR/RootSpy/
-#cp -R ../RootSpy-standalone/DRootSpy $TMPDIR/DRootSpy/
 
 # clean out bad files 
-rm $TMPDIR/RootSpy/make_dist_tarball.sh
-rm $TMPDIR/RootSpy/make_dist_tarball.sh~
+rm -f $TMPDIR/RootSpy/make_dist_tarball.sh
+rm -f $TMPDIR/RootSpy/make_dist_tarball.sh~
 
 # convert build files
 find $TMPDIR/RootSpy -name SConstruct -exec perl -pi -e 's/halld/rootspy/g' '{}' \;
